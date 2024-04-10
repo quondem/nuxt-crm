843c39c2a322254f6fcc54d08835de585a284977
quondem <86314992+quondem@users.noreply.github.com>
2024-04-10 17:57:25 +0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: false },
-	css: ["~/assets/css/main.css"],
+	css: ["~/assets/css/tailwind.css"],
 
 	postcss: {
 		plugins: {
